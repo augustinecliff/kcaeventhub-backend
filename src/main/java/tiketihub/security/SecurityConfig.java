53fe6e501efea08d5d8a8889e39e2da952539123
Augustine Cliff <augustinecliff10@gmail.com>
2023-07-16 23:32:35 +0300

    Setting up My database schema.

--- src/main/java/tiketihub/security/SecurityConfig.java
+++ src/main/java/tiketihub/security/SecurityConfig.java
@@ -1,8 +1,23 @@
 package tiketihub.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
-
+    @Bean
+    @Bean
+    public SecurityFilterChain(HttpSecurity http) {
+        http
+                .authorizeHttpRequests()
+                .requestMatchers()
+                .hasRole()
+                .anyRequest().permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/home")
+                .defaultSuccessUrl("")
+    }
 }
